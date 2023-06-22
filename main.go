79c1a5e1a2e71872d72593fd28299fc61ab37b7b
Stephen Sullivan <stephenmsullivan@gmail.com>
2023-06-22 14:44:30 -0700

    Update main.go
    add leaderboard creation

--- main.go
+++ main.go
@@ -8,5 +8,8 @@ import (
 
 func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
     logger.Info("Hello World!")
+
+    CreateLeaderboard(ctx, logger, db, nk, initializer)
+    
     return nil
 }
