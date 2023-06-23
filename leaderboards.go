f2db8ce8dffaf77776fcac361d39f915fa51a9b8
Stephen Sullivan <stephen@luckyjackpotcasino.com>
2023-06-22 19:45:59 -0700

    fix syntax

--- leaderboards.go
+++ leaderboards.go
@@ -7,18 +7,19 @@ import (
 	"github.com/heroiclabs/nakama-common/runtime"
 )
 
-func CreateLeaderboard(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error
-{
-    id := "multihandpoker_jackpots"
-    authoritative := false
-    sort := "desc"
-    operator := "set"
-	  reset := ""
-	  metadata := map[string]interface{}{}
+func CreateLeaderboard(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
 
-  if err := nk.LeaderboardCreate(ctx, id, authoritative, sort, operator, reset, metadata); err != nil {
-      // Handle error.
-     logger.Info("Leadboard Creation Failed!")
-  }
-  
+	id := "multihandpoker_jackpots"
+	authoritative := false
+	sort := "desc"
+	operator := "set"
+	reset := ""
+	metadata := map[string]interface{}{}
+
+	if err := nk.LeaderboardCreate(ctx, id, authoritative, sort, operator, reset, metadata); err != nil {
+		// Handle error.
+		logger.Info("Leadboard Creation Failed!")
+	}
+
+	return nil
 }
