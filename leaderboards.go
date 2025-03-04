4a8dba4401f92d52b4a234df359137f8985d5475
Stephen Sullivan <stephen@luckyjackpotcasino.com>
2025-03-04 13:18:19 -0800

    latest leaderboard API

--- leaderboards.go
+++ leaderboards.go
@@ -16,7 +16,7 @@ func CreateLeaderboard(name string, ctx context.Context, logger runtime.Logger, 
 	reset := ""
 	metadata := map[string]interface{}{}
 
-	if err := nk.LeaderboardCreate(ctx, id, authoritative, sort, operator, reset, metadata); err != nil {
+	if err := nk.LeaderboardCreate(ctx, id, authoritative, sort, operator, reset, metadata, false); err != nil {
 		// Handle error.
 		logger.Info("Leadboard Creation Failed!")
 	}
